22b6ab440df8db7160098f60b52c2666ea34ba7d
Andrew Melo <andrew.melo@gmail.com>
2011-03-15 20:37:46 -0500

    removing working data for null edges

--- ScriptGraph/Graph/NullEdge.py
+++ ScriptGraph/Graph/NullEdge.py
@@ -11,24 +11,30 @@ import commands
 
 nullCounter = 0
 class NullEdge( Edge.Edge ):
-	
-	def __init__( self, name = "" ):
-		global nullCounter
-		if not name:
-			Edge.Edge.__init__(self, name = "nullEdge%s" % nullCounter )
-			nullCounter += 1
-		Edge.Edge.__init__(self)
+    
+    def __init__( self, name = "" ):
+        global nullCounter
+        if not name:
+            Edge.Edge.__init__(self, name = "nullEdge%s" % nullCounter )
+            nullCounter += 1
+        Edge.Edge.__init__(self)
+    
+    def makeWorkDir( self ):
+        pass
+    
+    def executeImpl( self, fileList, fileMapList ):
+        pass
+    
+    def checkStatus( self ):
+        self.checkStatusImpl()
 
-	def executeImpl( self, fileList, fileMapList ):
-		pass
-	
-	def checkStatusImpl( self ):
-		parentFiles,_ = self.getParent().getFiles()
-		self.clearOutputFiles()
-		for file in parentFiles:
-			self.addOutputFile( file )
-		
-		if self.getParent().isReady():
-			self.setComplete()
-		else:
-			self.setIncomplete()
+    def checkStatusImpl( self ):
+        parentFiles,_ = self.getParent().getFiles()
+        self.clearOutputFiles()
+        for file in parentFiles:
+            self.addOutputFile( file )
+        
+        if self.getParent().isReady():
+            self.setComplete()
+        else:
+            self.setIncomplete()
