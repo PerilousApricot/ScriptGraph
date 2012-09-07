a5711a3a7084fe9ee335b265edc2e5023fbb840f
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2012-09-07 14:23:33 -0500

    updating setup.sh to be relocatable

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-export PATH=`pwd`/bin:$PATH
-export PYTHONPATH=`pwd`:$PYTHONPATH
+
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+export PATH=$DIR/bin:$PATH
+export PYTHONPATH=$DIR:$PYTHONPATH
+
+echo "Configuring ScriptGraph....."
+echo "Direct any complaints to andrew.melo@gmail.com"
